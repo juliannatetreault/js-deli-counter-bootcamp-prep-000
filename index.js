9fddd20292ebe0165ab0e13c0c1875c258986c4d
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-25 07:34:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function currentLine (katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return ("The line is currently empty.")
   } else {
-      return (`The line is currently: 1. ${0}, 2. ${1}, 3. ${2}.`);
+      return (`The line is currently: 1. ${Bill}, 2. ${Jane}, 3. ${Ann}.`);
   }
 }
